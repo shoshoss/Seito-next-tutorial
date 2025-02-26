23268ad637ef585a21eeffe09441c5cdac8b8395
shota <s8152.45@gmail.com>
2025-02-26 10:59:24 +0900

    Add: フォームの表示とスタイル

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Form.module.css";
+
+export default function Form() {
+	return (
+		<form className={styles.form}>
+			<div className={styles.formList}>
+				<label className={styles.formTitle} htmlFor="name">
+					Name:{" "}
+				</label>
+				<div className={styles.formField}>
+					<input type="text" id="name" />
+				</div>
+			</div>
+
+			<div className={styles.formList}>
+				<label className={styles.formTitle} htmlFor="task">
+					Task:{" "}
+				</label>
+				<div className={styles.formField}>
+					<input type="text" id="task" />
+				</div>
+			</div>
+
+			<div className={styles.formList}>
+				<label className={styles.formTitle} htmlFor="comment">
+					Comment:{" "}
+				</label>
+				<div className={styles.formField}>
+					<textarea name="comment" id="comment" rows="4" cols="50"></textarea>
+				</div>
+			</div>
+
+			<div className={styles.formButton}>
+				<button type="submit">Submit</button>
+			</div>
+		</form>
+	);
+}
