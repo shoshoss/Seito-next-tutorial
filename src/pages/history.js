b6f1c840c380136af83da33b675d59cab7dd4da2
shota <s8152.45@gmail.com>
2025-02-27 13:35:35 +0900

    Add: DBからデータを取得し表へ表示

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,14 +1,31 @@
 import { collection, getDocs } from "firebase/firestore";
 import Table from "@/components/Table/Table";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { db } from "@/lib/firebaseConfig";
 
 export default function History() {
+	const [data, setData] = useState([]);
+
 	const fetchData = async () => {
 		const querySnapshot = await getDocs(collection(db, "reports"));
+		const historyData = [];
 		querySnapshot.forEach((doc) => {
-			console.log(`${doc.id} => ${doc.data().comment}`);
+			// console.log(`${doc.id} => ${doc.data().comment}`);
+			const date = doc
+				.data()
+				.date.toDate()
+				.toISOString()
+				.replace("T", " ")
+				.slice(0, -8);
+			historyData.push({
+				id: doc.id,
+				date: date,
+				name: doc.data().name,
+				task: doc.data().task,
+				comment: doc.data().comment,
+			});
 		});
+		setData(historyData);
 	};
 
 	useEffect(() => {
@@ -17,7 +34,7 @@ export default function History() {
 
 	return (
 		<>
-			<Table />
+			<Table data={data} />
 		</>
 	);
 }
