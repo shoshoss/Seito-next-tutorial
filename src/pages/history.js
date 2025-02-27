30b5b528929912c0e2c942693658680de45fa5c2
shota <s8152.45@gmail.com>
2025-02-27 11:31:54 +0900

    Add: データの読み込みの確認

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,6 +1,20 @@
+import { collection, getDocs } from "firebase/firestore";
 import Table from "@/components/Table/Table";
+import { useEffect } from "react";
+import { db } from "@/lib/firebaseConfig";
 
 export default function History() {
+	const fetchData = async () => {
+		const querySnapshot = await getDocs(collection(db, "reports"));
+		querySnapshot.forEach((doc) => {
+			console.log(`${doc.id} => ${doc.data().comment}`);
+		});
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<Table />
