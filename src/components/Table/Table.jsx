10e134fa7a6dce1f1e481cf2f1a756d2bc934e59
shota <s8152.45@gmail.com>
2025-02-27 10:22:33 +0900

    Add: スタイルとmapでの表示

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import styles from "./Table.module.css";
+
+const dummyData = [
+	{
+		date: "2021-09-29",
+		name: "John Doe",
+		task: "Task 1",
+		comment: "Comment 1",
+	},
+	{
+		date: "2021-09-30",
+		name: "Jane Doe",
+		task: "Task 2",
+		comment: "Comment 2",
+	},
+];
 
 export default function Table() {
 	return (
-		<table>
-			<thead>
+		<table className={styles.table}>
+			<thead className={styles.tableHead}>
 				<tr>
 					<th>Date</th>
 					<th>Name</th>
@@ -11,19 +27,15 @@ export default function Table() {
 					<th>Comment</th>
 				</tr>
 			</thead>
-			<tbody>
-				<tr>
-					<td>2021-09-29</td>
-					<td>John Doe</td>
-					<td>Task 1</td>
-					<td>Comment 1</td>
-				</tr>
-				<tr>
-					<td>2021-09-30</td>
-					<td>Jane Doe</td>
-					<td>Task 2</td>
-					<td>Comment 2</td>
-				</tr>
+			<tbody className={styles.tableBody}>
+				{dummyData.map((item) => (
+					<tr>
+						<td>{item.date}</td>
+						<td>{item.name}</td>
+						<td>{item.task}</td>
+						<td>{item.comment}</td>
+					</tr>
+				))}
 			</tbody>
 		</table>
 	);
